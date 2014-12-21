c31fbc2411c9a2d6056af0d8819c778e6d93cdda
Alexis Hassler <alexis.hassler@sewatech.fr>
2014-12-21 22:43:00 +0100

    Checking if subsystem exists

--- app/app.js
+++ app/app.js
@@ -12,5 +12,18 @@ angular.module('flyNg',
   ]
 ).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/ds'});
+//  $routeProvider.otherwise({redirectTo: '/ds'});
+}]).
+controller('MenuController', ['$scope', '$location', 'management', function($scope, $location, management) {
+  var resources = new Array();
+
+  management.invoke('read-children-names', [], {"child-type": "subsystem"}).then(
+      function(data) {
+        resources = data.result;
+      }
+  );
+
+  $scope.hasSubsystem = function (name) {
+    return (resources.indexOf(name) >= 0);
+  }
 }]);
