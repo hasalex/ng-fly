9969dc5ed5fd7352f0f042bf3fbba21f4a7a3f2a
Alexis Hassler <alexis.hassler@sewatech.fr>
2015-03-24 19:00:01 +0100

    Add support for cassandra keyspaces

--- app/cassandra/cassandra.js
+++ app/cassandra/cassandra.js
@@ -15,11 +15,90 @@ angular
     .controller('CassandraController', ['$scope', '$log', '$location', 'management', function ($scope, $log, $location, management) {
 
         $scope.management = management;
-        management.initPage([ { "subsystem": "cassandra" } ], "cluster");
+        management.initPage([ { "subsystem": "cassandra" } ], "cluster")
+            .then(loadKeyspaces);
 
         $scope.create = function(result) {
 //            management.resource['driver-name'] = result.name;
             return management.create(result.name, management.resource);
         };
 
+        $scope.select = function() {
+            $scope.keyspaces = null;
+            $scope.keyspace = null;
+            $location.search('name', management.name);
+            load();
+        };
+
+        function load() {
+            return management.load()
+                .then(loadKeyspaces);
+        }
+
+        function loadKeyspaces() {
+            if (management.name !== null) {
+                return management.list([ {"subsystem": "cassandra"}, {"cluster": management.name}], 'keyspace').then(
+                    function(data) {
+                        $scope.keyspaceNames = data.result;
+                    },
+                    function(reason) {
+                        $scope.keyspaceNames = null;
+                    }
+                );
+            }
+        }
+
+        $scope.selectKeyspace = function(name) {
+            $scope.keyspaceName = name;
+            return management.invoke('read-resource', $scope.keyspaceAddress()).then(
+                function(data) {
+                    $scope.keyspace = data.result;
+                },
+                function() {
+                    $scope.keyspace = null;
+                }
+            );
+        };
+
+        $scope.removeKeyspace = function(name) {
+            // remove does not work
+            return management
+                .invoke('drop-keyspace', [ {"subsystem": "cassandra"}, {"cluster": management.name}], {name: name})
+                .then(function() {
+                    $scope.keyspaceName = null;
+                    $scope.keyspace = null;
+                })
+                .then(loadKeyspaces,
+                    function(data) {
+                        management.processError(data, data.status);
+                    });
+        };
+
+        $scope.addKeyspace = function() {
+            $scope.keyspaceName = null;
+            $scope.keyspace = {class: "org.apache.cassandra.locator.SimpleStrategy", replication_factor: 1};
+        }
+
+
+        $scope.createKeyspace = function() {
+            return management
+                .invoke('create-keyspace', [ {"subsystem": "cassandra"}, {"cluster": management.name}], $scope.keyspace)
+                .then(function() {
+                    $scope.keyspaceName = $scope.keyspace.name;
+                })
+                .then(loadKeyspaces)
+;
+        };
+
+        $scope.keyspaceAddress = function() {
+            var address = [{"subsystem": "cassandra"},
+                {"cluster": management.name},
+                {"keyspace": $scope.keyspaceName}];
+            return address;
+        };
+
+
+        $scope.active = function(keyspaceName) {
+            return (angular.equals($scope.keyspaceName, keyspaceName) ? 'active' : '');
+        };
     }]);
